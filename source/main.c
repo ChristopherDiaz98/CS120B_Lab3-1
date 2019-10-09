f9eb2aa6bc4fad5d9e8bde191c7d53427d3102b2
Jacob Halvorson <jhalvorson6687@icloud.com>
2019-10-08 17:33:33 -0700

    updated code for part 2

--- source/main.c
+++ source/main.c
@@ -1,8 +1,8 @@
 /*	Author: jhalv001
  *  Partner(s) Name: Chris Diaz
  *	Lab Section: 22
- *	Assignment: Lab 3  Exercise 1
- *	Exercise Description: Count the number of 1's on Ports A and B and outpu			      t that number on Port C. 
+ *	Assignment: Lab 3  Exercise 2
+ *	Exercise Description: Car fuel-level sensor. 
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
@@ -15,37 +15,41 @@
 int main(void) {
     /* Insert DDR and PORT initializations */
     DDRA = 0x00; PORTA = 0xFF;
-    DDRB = 0x00; PORTB = 0xFF;
     DDRC = 0xFF; PORTC = 0x00;
     	
     /* Insert your solution below */
-    unsigned char oneCnt = 0x00;
+    unsigned fuelLevel  = 0x00;
     unsigned char tmp = 0x00;
-    unsigned char i = 0;
+    unsigned char lowFuel = 0;
 
     while (1) {
 
-        PORTC = 0x00;
+        fuelLevel = 0x00;
         tmp = 0xFF & PINA;
+	lowFuel = 0x00;
 
-	for (i = 0; i < 8; i++) { 
-		if ((tmp & 0x01) == 0x01) {
-			oneCnt++;
-		}
-		tmp = tmp >> 1;
+	if ((tmp == 0x01) || (tmp == 0x02)) {
+		fuelLevel = 0x20;
+		lowFuel = 0x40;
+	}
+	else if ((tmp == 0x03) || (tmp == 0x04)) {
+		fuelLevel = 0x30;
+		lowFuel = 0x40;
+	}
+	else if ((tmp == 0x05) || (tmp ==0x06)) {
+		fuelLevel = 38;
+	}
+	else if ((tmp == 0x07) || (tmp == 0x08) || (tmp == 0x09)) {
+		fuelLevel = 0x3C;
+	}
+	else if ((tmp == 0x0A) || (tmp == 0x0B) || (tmp == 0x0C)) {
+		fuelLevel = 0x3E;
+	}
+	else {
+		fuelLevel = 0x3F;
 	}
 	
-	tmp = 0xFF & PINB;
-
-	for (i = 0; i < 8; i++) {
-		if ((tmp & 0x01) == 0x01) {
-			oneCnt++;
-		}
-		tmp = tmp >> 1;
-	}
-	
-	PORTC = oneCnt;
-	oneCnt = 0x00;      
+	PORTC = fuelLevel | lowFuel;
     }
     return 1;
 }
